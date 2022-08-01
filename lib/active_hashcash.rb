d52ad711a34c9cb2e8394a28574959c042a594b3
Alexis Bernard <alexis@basesecrete.com>
2022-08-01 10:48:16 +0200

    Move engine into its own file

--- lib/active_hashcash.rb
+++ lib/active_hashcash.rb
@@ -75,25 +75,8 @@ module ActiveHashcash
   def hashcash_redis_url
     ActiveHashcash.redis_url || ENV["ACTIVE_HASHCASH_REDIS_URL"] || ENV["REDIS_URL"]
   end
-
-  class Engine < ::Rails::Engine
-    config.assets.paths << File.expand_path("..", __FILE__)
-
-    config.after_initialize { load_translations }
-
-    def load_translations
-      if !I18n.backend.exists?(I18n.locale, "active_hashcash")
-        I18n.backend.store_translations(:de, {active_hashcash: {waiting_label: "Warten auf die Überprüfung ..."}})
-        I18n.backend.store_translations(:en, {active_hashcash: {waiting_label: "Waiting for verification ..."}})
-        I18n.backend.store_translations(:es, {active_hashcash: {waiting_label: "A la espera de la verificación ..."}})
-        I18n.backend.store_translations(:fr, {active_hashcash: {waiting_label: "En attente de vérification ..."}})
-        I18n.backend.store_translations(:it, {active_hashcash: {waiting_label: "In attesa di verifica ..."}})
-        I18n.backend.store_translations(:jp, {active_hashcash: {waiting_label: "検証待ち ..."}})
-        I18n.backend.store_translations(:pt, {active_hashcash: {waiting_label: "À espera de verificação ..."}})
-      end
-    end
-  end
 end
 
 require "active_hashcash/stamp"
 require "active_hashcash/store"
+require "active_hashcash/engine"
