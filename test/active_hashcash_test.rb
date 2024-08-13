fdb3019c119256a20bec0ef41de653d171b236e0
Jonas S <jonas_s@posteo.de>
2024-08-13 11:10:07 +0200

    remove ActionView::Helpers::FormTagHelper from ActiveHashcash

--- test/active_hashcash_test.rb
+++ test/active_hashcash_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require 'minitest/mock'
 
 class ActiveHashcashTest < ActiveSupport::TestCase
 
@@ -37,5 +38,16 @@ class ActiveHashcashTest < ActiveSupport::TestCase
 
     ActiveHashcash::Stamp.parse("1:20:220623:test::MPWRGuN3itbd1NiQ:008").update!(ip_address: "127.0.0.1")
     assert_equal(ActiveHashcash.bits + 3, controller.hashcash_bits)
+  end
+
+  def test_hashcash_hidden_field_tag
+    controller = SampleController.new
+    controller.request = Minitest::Mock.new
+    controller.request.expect :host, 'foo.test'
+
+    options = { resource: 'foo.test', bits: ActiveHashcash.bits, waiting_message: I18n.t("active_hashcash.waiting_label") }
+    options = ERB::Util.html_escape(options.to_json)
+    field = "<input type=\"hidden\" name=\"hashcash\" id=\"hashcash\" value=\"\" data-hashcash=\"#{options}\" autocomplete=\"off\" />"
+    assert_equal(controller.hashcash_hidden_field_tag, field)
   end
 end
