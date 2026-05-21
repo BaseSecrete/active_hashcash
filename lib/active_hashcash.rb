1fe416d92a2232718b02c93ad2fc157255bad900
Alexis Bernard <alexis@basesecrete.com>
2026-05-21 10:52:13 +0200

    Fix stamp date mismatch by using server date instead of client

--- lib/active_hashcash.rb
+++ lib/active_hashcash.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "active_hashcash/version"
 require "active_hashcash/engine"
 
@@ -118,7 +120,12 @@ module ActiveHashcash
   #   <% end %>
   #
   def hashcash_hidden_field_tag(name = :hashcash)
-    options = {resource: hashcash_resource, bits: hashcash_bits, waiting_message: hashcash_waiting_message}
+    options = {
+      resource: hashcash_resource,
+      bits: hashcash_bits,
+      waiting_message: hashcash_waiting_message,
+      date: Date.today.strftime("%y%m%d")
+    }
     view_context.hidden_field_tag(name, "", "data-hashcash" => options.to_json)
   end
 end
