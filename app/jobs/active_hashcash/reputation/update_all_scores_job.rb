c6c248e06d74d50a000891a63cfd4fb400525210
Alexis Bernard <alexis@basesecrete.com>
2026-09-04 15:42:41 +0200

    Freeze default jobs

--- app/jobs/active_hashcash/reputation/update_all_scores_job.rb
+++ app/jobs/active_hashcash/reputation/update_all_scores_job.rb
@@ -3,9 +3,9 @@
 module ActiveHashcash
   module Reputation
     class UpdateAllScoresJob < ApplicationJob
-      DEFAULT_JOBS = [UpdateAbuseScoreJob, UpdateAnonymousScoreJob, UpdateAttackScoreJob, CleanupJob]
+      DEFAULT_JOBS = [UpdateAbuseScoreJob, UpdateAnonymousScoreJob, UpdateAttackScoreJob, CleanupJob].freeze
 
-      def perform(jobs =  DEFAULT_JOBS)
+      def perform(jobs = DEFAULT_JOBS.dup)
         if (job = jobs.shift)
           job.perform_now
           self.class.perform_later(jobs) if jobs.any?
